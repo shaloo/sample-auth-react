c1977375e8bda2b694eb6302184908ed3c9be8c3
shaloo <5890484+shaloo@users.noreply.github.com>
2023-10-09 19:06:38 +0530

    upgrade to Auth v1.0.8

--- src/index.js
+++ src/index.js
@@ -8,11 +8,17 @@ import App from "./App";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 const provider = new AuthProvider(
-  //"xar_test_d24f70cd300823953dfa2a7f5b7c7c113356b1ad" //dev testnet
-  //wallet address
-  //"xar_dev_19527cdf585cd31d0bd06bfc1b008accea781404"
-  //wallet address 0xD12E6864A0f0f3Ea886400Ae7570E4341889bDa9
-  "xar_dev_5fd5338ee82834d59eee58f37b676bdabdfa41e9"
+  "xar_dev_19527cdf585cd31d0bd06bfc1b008accea781404",
+  { 
+    network: "dev",
+    theme: "dark",
+    connectOptions: {
+      compact: true
+    },
+    chainConfig: {
+      chainId: "80001"
+    }
+  }
 ); //See SDK Reference Guide for optional parameters
 
 root.render(
